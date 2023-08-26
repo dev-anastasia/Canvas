9c488e6181221fa3111ef7078e0f8589a3b5dcbb
Anastasia Stepanova <stepanovawm@gmail.com>
2023-08-26 10:50:32 +0300

    Починено! Работает! Но теперь нужно сделать рандомное количество шариков

--- app/src/main/java/com/example/baloons/DrawLayer.kt
+++ app/src/main/java/com/example/baloons/DrawLayer.kt
@@ -11,6 +11,7 @@ class DrawLayer(context: Context) : View(context) {
 
     private val paint = Paint()
     private val random = Random()
+    private val randomCount = random.nextInt(10)
 
     private var radius = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP, 10F, resources.displayMetrics
@@ -20,23 +21,28 @@ class DrawLayer(context: Context) : View(context) {
     private val height = context.resources.displayMetrics.heightPixels
 
     // Первая точка
-    private val x0 = random.nextInt(width).toFloat()
-    private val y0 = random.nextInt(height).toFloat()
+    private var x0: Float = random.nextInt(width).toFloat()
+    private var y0: Float = random.nextInt(height).toFloat()
 
     // Вторая точка
-    private val x1 = random.nextInt(width).toFloat()
-    private val y1 = random.nextInt(height).toFloat()
+    private var x1: Float = random.nextInt(width).toFloat()
+    private var y1: Float = random.nextInt(height).toFloat()
 
     // Ищем k (угол) и b (шаг)
-    private val y = y1 - y0
-    private val x = x1 - x0
-    private var k: Float = y / x
-    private var b = y0 - k * x0
+    private var y = y1 - y0
+    private var x = x1 - x0
+    private var k: Float = (
+            if (x != 0.0f)
+                y / x
+            else
+                0.0f
+            )
+    //private var b = y0 - k * x0 // получается, не нужен?
 
-    private var shift = 5
+    private var xShift = 5
+    private var yShift = 5
     private var xCur = x1
-    private var yCur: Float = y1
-
+    private var yCur = y1
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -44,16 +50,21 @@ class DrawLayer(context: Context) : View(context) {
         canvas?.drawColor(
             context.resources.getColor(R.color.white, context.resources.newTheme())
         )
-        paint.color = context.resources.getColor(R.color.black, context.resources.newTheme())
 
-        if (yCur <= 0 || yCur >= height || xCur <= 0 || xCur >= width) {
+        if (xCur <= 0 + radius || xCur >= width - radius) {
             k *= -1
-            shift *= -1
-            b = 2 * k * xCur + b
+            xShift *= -1
         }
-        xCur += shift
-        yCur = k * xCur + b
 
+        if (yCur <= 0 + radius || yCur >= height - radius * 3) {
+            k *= -1
+            yShift *= -1
+        }
+
+        xCur += xShift
+        yCur += yShift
+
+        paint.color = context.resources.getColor(R.color.black, context.resources.newTheme())
         canvas?.drawCircle(xCur, yCur, radius, paint)
 
         postInvalidate()
